892ac0f460a8149b11d9ee420450ed8b3d8f362f
V.Gayathri <136567179+gayathri-4A1@users.noreply.github.com>
2025-11-16 19:54:14 +0530

    Add files via upload

---
+++ task2.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE Task2;
+USE Task2;
+
+CREATE TABLE employees(
+	id INT PRIMARY KEY,
+    name VARCHAR(50),
+    salary INT
+);
+
+INSERT INTO employees VALUES(1, 'Gayathri' , 30000);
+INSERT INTO employees VALUES(2, 'Priya' ,35000);
+INSERT INTO employees VALUES(3,  'Kavya', 40000);
+
+
+
